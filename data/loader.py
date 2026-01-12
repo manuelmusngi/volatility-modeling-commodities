203b5897f691497932054ceb7a9fa8919113d20c
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2026-01-12 17:11:02 -0500

    Delete data directory

--- data/loader.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-import numpy as np
-import pandas as pd
-
-def fetch_data(ticker, start, end):
-    df = yf.download(ticker, start=start, end=end)
-    df['Return'] = 100 * np.log(df['Adj Close'] / df['Adj Close'].shift(1))
-    df.dropna(inplace=True)
-    return df[['Adj Close', 'Return']]
